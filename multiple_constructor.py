887908f36118640fa640a58460130b3bbaf3b5ae
dlvvs1 <danduleeladlvvs1@gmail.com>
2026-01-20 21:02:08 +0530

    multiple constructor?

--- multiple_constructor.py
+++ multiple_constructor.py
@@ -7,3 +7,7 @@ class student:
         print("the student name is {} and age is {}".format(self.name , self.age))
 
 S = student("dandu" , 21) 
+
+# here the second __init__ method will override the first __init__ method.
+# constructor overloading is not possible in python like java.
+# but we can achieve the same using default arguments or *args and **kwargs.
