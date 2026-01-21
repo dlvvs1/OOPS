ed3e7b97e9d2f4ec212c357b8a405b2570883312
dlvvs1 <danduleeladlvvs1@gmail.com>
2026-01-21 16:20:56 +0530

    simple example of Methods

---
+++ methods.py
@@ -0,0 +1,6 @@
+class dog:  #class definition
+    def bark(self):  # Method of dog class
+        print("woof woof")
+
+d = dog() # object of dog class
+d.bark()  # calling the bark method of dog class
