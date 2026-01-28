6eee2eff598b1b4893aa68783e5bbcb793db0a3c
dlvvs1 <danduleeladlvvs1@gmail.com>
2026-01-28 18:37:59 +0530

    Getter and Setter decorators

---
+++ getter_and_setter.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, age):
+        self._age = age
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        self._age = value
+
+p = Person(25)
+p.age = 30
+print(p.age)
