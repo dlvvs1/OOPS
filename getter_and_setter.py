d7673817ae6e5742d2421f86ae96ad14d5f88e3f
dlvvs1 <danduleeladlvvs1@gmail.com>
2026-01-29 09:50:06 +0530

    getters and setters

--- getter_and_setter.py
+++ getter_and_setter.py
@@ -1,31 +1,16 @@
-# class Person:
-#     def __init__(self, age):
-#         self._age = age
+class Person:
+    def __init__(self, age):
+        self._age = age
 
-#     @property
-#     def age(self):
-#         return self._age
+    @property
+    def age(self):
+        return self._age
 
-#     @age.setter
-#     def age(self, value):
-#         self._age = value
+    @age.setter
+    def age(self, value):
+        self._age = value
 
-# p = Person(25)
-# p.age = 30
-# print(p.age)
+p = Person(25)
+p.age = 30
+print(p.age)
 
-class Animal:
-    def speak(self):
-        pass
-
-class Dog(Animal):
-    def speak(self):
-        return "Bark"
-
-class Cat(Animal):
-    def speak(self):
-        return "Meow"
-
-animals = [Dog(), Cat()]
-for a in animals:
-    print(a.speak())
